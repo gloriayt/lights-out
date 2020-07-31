00bff08599ea07711107eb07acf1b719d2dc021c
gloriayt <gloriaytai@gmail.com>
2020-08-01 01:28:34 +1000

    fixed title

--- build/precache-manifest.08fda1d33edf522cab2f086bc5512b86.js
+++ build/precache-manifest.08fda1d33edf522cab2f086bc5512b86.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
   },
   {
-    "revision": "2c8105396e0e421dd530",
-    "url": "/lights-out/static/js/main.2c810539.chunk.js"
+    "revision": "b5b6f63c08ff8f2dd2f6",
+    "url": "/lights-out/static/js/main.b5b6f63c.chunk.js"
   },
   {
     "revision": "45ea534042495dbb2393",
     "url": "/lights-out/static/js/1.45ea5340.chunk.js"
   },
   {
-    "revision": "2c8105396e0e421dd530",
-    "url": "/lights-out/static/css/main.9bccde57.chunk.css"
+    "revision": "b5b6f63c08ff8f2dd2f6",
+    "url": "/lights-out/static/css/main.6f26596f.chunk.css"
   },
   {
-    "revision": "a64a6e2124085e0e51447693f4c8fc21",
+    "revision": "29829e5dcb3f8ad4f8bc445fbb6dd638",
     "url": "/lights-out/index.html"
   }
 ];
